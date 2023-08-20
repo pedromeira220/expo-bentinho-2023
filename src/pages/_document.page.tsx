6391c2e23cf4b4029cb24019d929b2984e09dbb8
Pedro Meira <pedrohmeira@icloud.com>
2023-08-19 21:53:35 -0300

    initial commit

---
+++ src/pages/_document.page.tsx
@@ -0,0 +1,25 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="font-['Inter'_sans-serif] antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ src/pages/index.page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-purple-500 font-bold text-4xl">Hello world</h1>
+    </div>
+  )
+}
