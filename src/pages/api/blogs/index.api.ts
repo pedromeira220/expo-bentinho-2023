eab4bc9a7f783504cae424d8ae2d5b4f73f5e6c4
Pedro Meira <pedrohmeira@icloud.com>
2023-08-27 11:19:38 -0300

    feat: adding create blog api route

--- src/pages/api/blogs/index.api.ts
+++ src/pages/api/blogs/index.api.ts
@@ -1,6 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { prisma } from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { randomUUID } from 'node:crypto'
+
+import posts from '@/data/posts.json'
+
+type ImageCreateManyInput = {
+  id?: string
+  search_term?: string | null
+  src: string
+  section_id?: string | null
+}
+
+type PostCreateManyInput = {
+  id?: string
+  title: string
+  subtitle: string
+  blog_id: string
+  thumbnail_id: string
+  creation_date: Date | string
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -43,8 +62,11 @@ export default async function handler(
     })
   }
 
-  const user = await prisma.blog.create({
+  const blogId = randomUUID()
+
+  const blog = await prisma.blog.create({
     data: {
+      id: blogId,
       description,
       name,
       primary_color: primaryColor,
@@ -52,6 +74,44 @@ export default async function handler(
       theme,
       creation_date: new Date(),
     },
+  })
+
+  const imagesToCreate: (ImageCreateManyInput & { list_id: number })[] =
+    posts.map((post) => {
+      return {
+        id: String(randomUUID()),
+        src: 'https://github.com/pedromeira220.png',
+        search_term: post.thumbnail_search_term,
+        list_id: post.id,
+      }
+    })
+
+  await prisma.image.createMany({
+    data: imagesToCreate.map((image) => {
+      return {
+        ...image,
+        list_id: undefined,
+      }
+    }),
+  })
+
+  const postsToCreate: PostCreateManyInput[] = posts.map((post) => {
+    return {
+      creation_date: new Date(),
+      subtitle: post.subtitle,
+      title: post.name,
+      // thumbnail_id: String(imagesToCreate[index].id),
+      thumbnail_id: String(
+        imagesToCreate.find((image) => {
+          return image.list_id === post.id
+        })?.id,
+      ),
+      blog_id: blog.id,
+    }
+  })
+
+  await prisma.post.createMany({
+    data: postsToCreate,
   })
 
   return res.status(201).end()
