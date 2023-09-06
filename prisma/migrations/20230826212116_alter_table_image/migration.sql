826c079669165ee7350baf20c08705ecc3b783d1
Pedro Meira <pedrohmeira@icloud.com>
2023-09-05 21:11:58 -0300

    feat: removing all features and leaving only the blog

--- prisma/migrations/20230826212116_alter_table_image/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "content_images" DROP CONSTRAINT "content_images_section_id_fkey";
-
--- AlterTable
-ALTER TABLE "content_images" ALTER COLUMN "section_id" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "content_images" ADD CONSTRAINT "content_images_section_id_fkey" FOREIGN KEY ("section_id") REFERENCES "sections"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,45 +1,137 @@
-import { ContentCreatorSvg } from '@/components/ContentCreatorSvg'
-import { Header } from '@/components/Header'
-import * as Input from '@/components/Input'
-import { Sparkle } from '@phosphor-icons/react'
+import { Footer } from '@/components/Footer'
+import { Heading } from '@/components/Heading'
+import { Paragraph } from '@/components/Paragraph'
+import { WhiteSpace } from '@/components/WhiteSpace'
 
-export default function Home() {
+export default function BlogPost() {
   return (
     <div>
-      <Header />
-      <main className="w-full mt-16 mb-24">
-        <div className="max-w-6xl mx-auto grid grid-cols-2  items-center px-8">
-          <div>
-            <div className="flex flex-col gap-6">
-              <h1 className="text-gray-900 font-semibold text-5xl">
-                Construa seu blog com a agilidade da IA
-              </h1>
-              <p className="text-gray-500 text-xl">
-                Sua visão, poucas palavras: nossa IA transforma sua ideia em um
-                blog completo, com artigos de qualidade, em um instante
-              </p>
-            </div>
-
-            <div className="mt-12 flex gap-4">
-              <Input.Container className="flex-1">
-                <Input.Control placeholder="Digite o tema do blog" />
-              </Input.Container>
-
-              <button className="bg-primary-600 flex items-center justify-center py-3 px-5 rounded-lg gap-2 hover:bg-primary-700 transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-primary-500 outline-none">
-                <span className="text-white font-semibold">Gerar blog</span>
-                <Sparkle
-                  className="text-white text-base"
-                  size={16}
-                  weight="fill"
-                />
-              </button>
-            </div>
+      <main className="w-full mb-24">
+        <div className="grid grid-cols-2 gap-16 min-h-screen ">
+          <div className="flex flex-col gap-6 justify-center pl-28 pr-16 h-full ">
+            <h1 className="text-gray-900 font-semibold text-5xl">
+              Building your API Stack
+            </h1>
+            <Paragraph className="text-gray-600 text-xl">
+              The rise of RESTful APIs has been met by a rise in tools for
+              creating, testing, and managing them.
+            </Paragraph>
           </div>
-          <div>
-            <ContentCreatorSvg />
+          <div className="w-full h-full flex items-center justify-center">
+            <img
+              src="https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png"
+              className="aspect-square object-cover w-full"
+            />
+          </div>
+        </div>
+
+        <div className="max-w-[45rem] mx-auto mt-24">
+          <div className="mb-5">
+            <Heading>Introduction</Heading>
+            <WhiteSpace size={20} />
+          </div>
+
+          <Paragraph>
+            Mi tincidunt elit, id quisque ligula ac diam, amet. Vel etiam
+            suspendisse morbi eleifend faucibus eget vestibulum felis. Dictum
+            quis montes, sit sit. Tellus aliquam enim urna, etiam. Mauris
+            posuere vulputate arcu amet, vitae nisi, tellus tincidunt. At
+            feugiat sapien varius id.
+          </Paragraph>
+
+          <Paragraph>
+            Eget quis mi enim, leo lacinia pharetra, semper. Eget in volutpat
+            mollis at volutpat lectus velit, sed auctor. Porttitor fames arcu
+            quis fusce augue enim. Quis at habitant diam at. Suscipit tristique
+            risus, at donec. In turpis vel et quam imperdiet. Ipsum molestie
+            aliquet sodales id est ac volutpat.{' '}
+          </Paragraph>
+          <WhiteSpace size={48} />
+          <img
+            src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+            className="rounded-xl"
+          />
+          <WhiteSpace size={48} />
+          <Paragraph>
+            Mi tincidunt elit, id quisque ligula ac diam, amet. Vel etiam
+            suspendisse morbi eleifend faucibus eget vestibulum felis. Dictum
+            quis montes, sit sit. Tellus aliquam enim urna, etiam. Mauris
+            posuere vulputate arcu amet, vitae nisi, tellus tincidunt. At
+            feugiat sapien varius id.
+          </Paragraph>
+          <Paragraph>
+            Eget quis mi enim, leo lacinia pharetra, semper. Eget in volutpat
+            mollis at volutpat lectus velit, sed auctor. Porttitor fames arcu
+            quis fusce augue enim. Quis at habitant diam at. Suscipit tristique
+            risus, at donec. In turpis vel et quam imperdiet. Ipsum molestie
+            aliquet sodales id est ac volutpat.{' '}
+          </Paragraph>
+          <Paragraph>
+            Eget quis mi enim, leo lacinia pharetra, semper. Eget in volutpat
+            mollis at volutpat lectus velit, sed auctor. Porttitor fames arcu
+            quis fusce augue enim. Quis at habitant diam at. Suscipit tristique
+            risus, at donec. In turpis vel et quam imperdiet. Ipsum molestie
+            aliquet sodales id est ac volutpat.{' '}
+          </Paragraph>
+          <WhiteSpace size={32} />
+          <Heading size="h3">Software and tools</Heading>
+          <WhiteSpace size={16} />
+
+          <Paragraph>
+            Eget quis mi enim, leo lacinia pharetra, semper. Eget in volutpat
+            mollis at volutpat lectus velit, sed auctor. Porttitor fames arcu
+            quis fusce augue enim. Quis at habitant diam at. Suscipit tristique
+            risus, at donec. In turpis vel et quam imperdiet. Ipsum molestie
+            aliquet sodales id est ac volutpat.{' '}
+          </Paragraph>
+          <Paragraph>
+            Eget quis mi enim, leo lacinia pharetra, semper. Eget in volutpat
+            mollis at volutpat lectus velit, sed auctor. Porttitor fames arcu
+            quis fusce augue enim. Quis at habitant diam at. Suscipit tristique
+            risus, at donec. In turpis vel et quam imperdiet. Ipsum molestie
+            aliquet sodales id est ac volutpat.{' '}
+          </Paragraph>
+
+          <WhiteSpace size={32} />
+          <Heading size="h3">Software and tools</Heading>
+          <WhiteSpace size={16} />
+
+          <Paragraph>
+            Eget quis mi enim, leo lacinia pharetra, semper. Eget in volutpat
+            mollis at volutpat lectus velit, sed auctor. Porttitor fames arcu
+            quis fusce augue enim. Quis at habitant diam at. Suscipit tristique
+            risus, at donec. In turpis vel et quam imperdiet. Ipsum molestie
+            aliquet sodales id est ac volutpat.{' '}
+          </Paragraph>
+          <Paragraph>
+            Eget quis mi enim, leo lacinia pharetra, semper. Eget in volutpat
+            mollis at volutpat lectus velit, sed auctor. Porttitor fames arcu
+            quis fusce augue enim. Quis at habitant diam at. Suscipit tristique
+            risus, at donec. In turpis vel et quam imperdiet. Ipsum molestie
+            aliquet sodales id est ac volutpat.{' '}
+          </Paragraph>
+          <WhiteSpace size={48} />
+          <div className="bg-gray-50 p-8 rounded-2xl">
+            <Heading size="h2">Conclusion</Heading>
+            <WhiteSpace size={20} />
+            <Paragraph>
+              Eget quis mi enim, leo lacinia pharetra, semper. Eget in volutpat
+              mollis at volutpat lectus velit, sed auctor. Porttitor fames arcu
+              quis fusce augue enim. Quis at habitant diam at. Suscipit
+              tristique risus, at donec. In turpis vel et quam imperdiet. Ipsum
+              molestie aliquet sodales id est ac volutpat.{' '}
+            </Paragraph>
+            <Paragraph>
+              Eget quis mi enim, leo lacinia pharetra, semper. Eget in volutpat
+              mollis at volutpat lectus velit, sed auctor. Porttitor fames arcu
+              quis fusce augue enim. Quis at habitant diam at. Suscipit
+              tristique risus, at donec. In turpis vel et quam imperdiet. Ipsum
+              molestie aliquet sodales id est ac volutpat.{' '}
+            </Paragraph>
           </div>
         </div>
       </main>
+      <Footer />
     </div>
   )
 }
